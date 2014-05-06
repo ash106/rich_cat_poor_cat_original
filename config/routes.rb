3649174ca7f4fa240ca9cc3b682e8a54cabd5ab7
Alex Howington <alex.howington@gmail.com>
2014-05-06 02:55:54 -0600

    Add super basic homepage and change some CSS rules

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root :to => "visitors#index"
+  get 'home/index'
+
+  root 'home#index'
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 end
