510cfac8456968c4481f581dc823767ac2d00b7e
Alex Howington <alex.howington@gmail.com>
2014-05-06 19:00:50 -0600

    Create basic CRUD actions for Cat model

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,53 @@
 class CatsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @cats = Cat.all
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+  end
+
+  def new
+    @cat = Cat.new
+    @finances_enum = Cat.finances
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      redirect_to @cat, notice: "Cat successfully created!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+    @finances_enum = Cat.finances
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      redirect_to @cat, notice: "Cat successfully updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cat = Cat.find(params[:id])
+    @cat.destroy
+    redirect_to cats_url, notice: "Cat successfully deleted!"
+  end
+
+private
+  
+  def cat_params
+    params.require(:cat).permit(:finances, :image)
+  end
+
 end
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,6 +1,6 @@
 class Cat < ActiveRecord::Base
   enum finances: [ :rich, :poor ]
 
-  has_attached_file :image, :styles => { :large => "400x400", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "400x400>", :thumb => "100x100>" }, :default_url => "/images/missing_:style.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
