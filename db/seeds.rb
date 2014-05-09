cf44e8f420ec3b02e580ade1ca27987f8dedf329
Alex Howington <alex.howington@gmail.com>
2014-05-09 15:42:54 -0600

    Use image files hosted on my domain instead of locally

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,41 +9,41 @@ user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
 cat = Cat.new(finances: "rich", mask_params: "241, 267, 35")
-cat.image = File.open('/Users/alex/Desktop/cats/rich_cat_1.jpg')
+cat.image = open('http://alexhowington.com/cats/rich_cat_1.jpg')
 cat.save!
 
 cat = Cat.new(finances: "rich", mask_params: "212, 65, 65")
-cat.image = File.open('/Users/alex/Desktop/cats/rich_cat_2.jpg')
+cat.image = open('http://alexhowington.com/cats/rich_cat_2.jpg')
 cat.save!
 
 cat = Cat.new(finances: "rich", mask_params: "213, 175, 60")
-cat.image = File.open('/Users/alex/Desktop/cats/rich_cat_3.jpg')
+cat.image = open('http://alexhowington.com/cats/rich_cat_3.jpg')
 cat.save!
 
 cat = Cat.new(finances: "rich", mask_params: "160, 165, 37")
-cat.image = File.open('/Users/alex/Desktop/cats/rich_cat_4.jpg')
+cat.image = open('http://alexhowington.com/cats/rich_cat_4.jpg')
 cat.save!
 
 cat = Cat.new(finances: "rich", mask_params: "68, 185, 55")
-cat.image = File.open('/Users/alex/Desktop/cats/rich_cat_5.jpg')
+cat.image = open('http://alexhowington.com/cats/rich_cat_5.jpg')
 cat.save!
 
 cat = Cat.new(finances: "poor", mask_params: "328, 197, 75")
-cat.image = File.open('/Users/alex/Desktop/cats/poor_cat_1.jpg')
+cat.image = open('http://alexhowington.com/cats/poor_cat_1.jpg')
 cat.save!
 
 cat = Cat.new(finances: "poor", mask_params: "105, 225, 98")
-cat.image = File.open('/Users/alex/Desktop/cats/poor_cat_2.jpg')
+cat.image = open('http://alexhowington.com/cats/poor_cat_2.jpg')
 cat.save!
 
 cat = Cat.new(finances: "poor", mask_params: "232, 107, 55")
-cat.image = File.open('/Users/alex/Desktop/cats/poor_cat_3.jpg')
+cat.image = open('http://alexhowington.com/cats/poor_cat_3.jpg')
 cat.save!
 
 cat = Cat.new(finances: "poor", mask_params: "157, 196, 81")
-cat.image = File.open('/Users/alex/Desktop/cats/poor_cat_4.jpg')
+cat.image = open('http://alexhowington.com/cats/poor_cat_4.jpg')
 cat.save!
 
 cat = Cat.new(finances: "poor", mask_params: "158, 85, 47")
-cat.image = File.open('/Users/alex/Desktop/cats/poor_cat_5.jpg')
+cat.image = open('http://alexhowington.com/cats/poor_cat_5.jpg')
 cat.save!
